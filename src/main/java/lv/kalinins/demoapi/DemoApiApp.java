cf04961a5655e4565247150a45c123c50f7c5903
Aleksandrs Kalinins <alexvk1984@gmail.com>
2021-11-30 20:53:33 +0000

    Initial commit

---
+++ src/main/java/lv/kalinins/demoapi/DemoApiApp.java
@@ -0,0 +1,12 @@
+package lv.kalinins.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApp.class, args);
+    }
+}
