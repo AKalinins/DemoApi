41bdeac99be8042dc3778fe51889554cec976ba8
Aleksandrs Kalinins <alexvk1984@gmail.com>
2021-12-02 20:41:49 +0000

    Update sql scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 -- This scripts will run automatically when app starts to populate db with some test data
-INSERT INTO users(name) VALUES('Bob Martin');
+INSERT INTO users(name, type) VALUES('Bob Ross', 'PRIVATE');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,18 @@
-CREATE TABLE users(
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    name varchar(255)
+CREATE TABLE users
+(
+    id   bigint PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    type varchar(255) NOT NULL
+);
+
+CREATE TABLE contracts
+(
+    id         bigint PRIMARY KEY AUTO_INCREMENT,
+    user_id    bigint       NOT NULL,
+    type       varchar(255) NOT NULL,
+    start_date date         NOT NULL,
+    CONSTRAINT `FK_INSTRUCTOR`
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION
 );
